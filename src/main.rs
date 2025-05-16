53589b0463c978549b2529b618f008a2a6fbc55f
saschatran <transa@ethz.ch>
2025-05-16 18:26:38 +0200

    fmtlet

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
 use clap::Parser;
-use std::path::PathBuf;
 use log::*;
-use std::{env, fs};
+use std::path::PathBuf;
 use std::process::Command;
+use std::{env, fs};
 
 // ./reducer –query <query-to-minimize –test <an arbitrary-script>
 fn main() {
     let (args, pwd) = init();
 
-    let (querys, test_path) = read_and_parse_args(args,pwd);
-    test_query(test_path, "queries/query1/original_test.sql".parse().unwrap())
+    let (querys, test_path) = read_and_parse_args(args, pwd);
+    test_query(
+        test_path,
+        "queries/query1/original_test.sql".parse().unwrap(),
+    )
 }
 
 fn test_query(test: PathBuf, reduced_query_file: PathBuf) {
-
     let output = Command::new(test)
-            .arg(reduced_query_file)
-            .output()
-            .expect("failed to execute process");
+        .arg(reduced_query_file)
+        .output()
+        .expect("failed to execute process");
 
     info!("{:?}", output)
 }
 
 fn read_and_parse_args(args: Cli, pwd: PathBuf) -> (String, PathBuf) {
     let query_path = pwd.join(args.query);
-    info!("{:?}" ,query_path);
+    info!("{:?}", query_path);
     let query = fs::read_to_string(query_path)
         .expect("Should have been able to read the query file: `query_path`");
 
