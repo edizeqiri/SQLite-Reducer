cc44ca39cb2a7f783f9e1fb9250caaa08b3b9318
saschatran <transa@ethz.ch>
2025-05-27 08:18:22 +0000

    merged

--- src/reducer.rs
+++ src/reducer.rs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 use crate::delta_debug::delta_debug;
 use crate::driver;
 use crate::transformation;
@@ -19,3 +20,42 @@ pub fn reduce(current_ast: Vec<Statement>) -> Result<Vec<Statement>, Box<dyn std
     );
     minimal_stmt
 }
+=======
+use crate::delta_debug::delta_debug;
+use crate::driver;
+use crate::transformation;
+use crate::utils::vec_statement_to_string;
+use log::info;
+use sqlparser::ast::Statement;
+use std::io::{Error, Read};
+use std::string::ParseError;
+use transformation::transformer;
+
+pub fn reduce(ast: Vec<Statement>) -> Result<String, Error> {
+    //info!("{:?}", ast[0].to_string());
+    let trans = transformer::transform(ast.clone());
+    info!("Transformation is : {:?}", trans);
+    Ok("Print".to_string())
+}
+
+pub fn reduce_statements(
+    current_ast: Vec<Statement>,
+) -> Result<Vec<Statement>, Box<dyn std::error::Error>> {
+    let current_ast_length = current_ast.len(); // get length before passing ownership
+    let minimal_stmt = delta_debug(current_ast, 2);
+    if let Ok(statements) = &minimal_stmt {
+        println!(
+            "REDUCED STATEMENT: {}",
+            vec_statement_to_string(statements, ";")
+        )
+    } else {
+        println!("REDUCED STATEMENT: Failed to get statements");
+    }
+    info!(
+        "original query length: {:?}, reduced query length: {:?}",
+        current_ast_length,
+        minimal_stmt.as_ref().map(|v| v.len())
+    );
+    minimal_stmt
+}
+>>>>>>> Stashed changes
